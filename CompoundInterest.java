bac959ce3e305cecb0ca5b98e5326a088ceb28e7
tejaswigupta <60374652+tejaswigupta@users.noreply.github.com>
2020-07-19 15:41:27 +0530

    Add files via upload

---
+++ CompoundInterest.java
@@ -0,0 +1,7 @@
+public class CompoundInterest
+{
+	double compoundInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return (principle*Math.pow(1+(rateOfInterest/100),time));
+	}
+}
---
+++ SimpleInterest.java
@@ -0,0 +1,7 @@
+public class SimpleInterest
+{
+	float simpleInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return ((principle*time*rateOfInterest)/100);
+	}
+}
